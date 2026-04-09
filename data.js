3bd831fc94202853d3e9518c49dec10eb1db5d20
Djmanny <Djmanny@RISRISH20030688.central.co.th>
2026-04-09 12:15:41 +0700

    Deploy Modernized Dashboard to Root for GitHub Pages

---
+++ data.js
@@ -0,0 +1,244 @@
+// Automatically generated at 2026-04-09 12:15:32
+const DASHBOARD_DATA = {
+    "Branch": {
+        "1.1": {
+            "Q1": {
+                "total": 10,
+                "success": 0
+            },
+            "Q2": {
+                "total": 145,
+                "success": 8
+            }
+        },
+        "1.2": {
+            "Q2": {
+                "total": 30,
+                "success": 0
+            }
+        },
+        "2": {
+            "Q1": {
+                "total": 3578,
+                "success": 0
+            },
+            "Q2": {
+                "total": 60,
+                "success": 0
+            }
+        },
+        "3": {
+            "Q1": {
+                "total": 4,
+                "success": 0
+            },
+            "Q2": {
+                "total": 86,
+                "success": 0
+            }
+        },
+        "4": {
+            "Q1": {
+                "total": 117,
+                "success": 0
+            },
+            "Q2": {
+                "total": 71,
+                "success": 0
+            }
+        },
+        "5": {
+            "Q1": {
+                "total": 348,
+                "success": 0
+            },
+            "Q2": {
+                "total": 2894,
+                "success": 0
+            }
+        },
+        "6": {
+            "Q1": {
+                "total": 21,
+                "success": 0
+            },
+            "Q2": {
+                "total": 145,
+                "success": 0
+            }
+        },
+        "7": {
+            "Q1": {
+                "total": 237,
+                "success": 0
+            },
+            "Q2": {
+                "total": 625,
+                "success": 0
+            }
+        },
+        "8": {
+            "Q2": {
+                "total": 3,
+                "success": 0
+            }
+        }
+    },
+    "DC": {
+        "1.1": {
+            "Q2": {
+                "total": 31,
+                "success": 5
+            }
+        },
+        "1.2": {
+            "Q2": {
+                "total": 5,
+                "success": 0
+            }
+        },
+        "2": {
+            "Q1": {
+                "total": 591,
+                "success": 0
+            },
+            "Q2": {
+                "total": 8,
+                "success": 0
+            }
+        },
+        "3": {
+            "Q1": {
+                "total": 1,
+                "success": 0
+            },
+            "Q2": {
+                "total": 35,
+                "success": 0
+            }
+        },
+        "4": {
+            "Q1": {
+                "total": 6,
+                "success": 0
+            },
+            "Q2": {
+                "total": 27,
+                "success": 0
+            }
+        },
+        "5": {
+            "Q1": {
+                "total": 21,
+                "success": 0
+            },
+            "Q2": {
+                "total": 22,
+                "success": 0
+            }
+        },
+        "6": {
+            "Q1": {
+                "total": 2,
+                "success": 0
+            },
+            "Q2": {
+                "total": 1,
+                "success": 0
+            }
+        },
+        "7": {
+            "Q1": {
+                "total": 127,
+                "success": 0
+            },
+            "Q2": {
+                "total": 106,
+                "success": 0
+            }
+        },
+        "8": {
+            "Q2": {
+                "total": 3,
+                "success": 0
+            }
+        }
+    },
+    "HO": {
+        "1.1": {
+            "Q2": {
+                "total": 15,
+                "success": 4
+            }
+        },
+        "1.2": {
+            "Q2": {
+                "total": 14,
+                "success": 0
+            }
+        },
+        "2": {
+            "Q1": {
+                "total": 1432,
+                "success": 0
+            },
+            "Q2": {
+                "total": 36,
+                "success": 0
+            }
+        },
+        "3": {
+            "Q1": {
+                "total": 15,
+                "success": 0
+            },
+            "Q2": {
+                "total": 182,
+                "success": 0
+            }
+        },
+        "4": {
+            "Q2": {
+                "total": 68,
+                "success": 0
+            }
+        },
+        "5": {
+            "Q1": {
+                "total": 19,
+                "success": 0
+            },
+            "Q2": {
+                "total": 59,
+                "success": 0
+            }
+        },
+        "6": {
+            "Q1": {
+                "total": 10,
+                "success": 0
+            },
+            "Q2": {
+                "total": 21,
+                "success": 0
+            }
+        },
+        "7": {
+            "Q1": {
+                "total": 78,
+                "success": 0
+            },
+            "Q2": {
+                "total": 509,
+                "success": 0
+            }
+        },
+        "8": {
+            "Q2": {
+                "total": 3,
+                "success": 0
+            }
+        }
+    }
+};
+const LAST_UPDATED = '2026-04-09 12:15:32';
