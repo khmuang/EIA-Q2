52b9c6c6d71bf0fb74dd57e01129d7ecb623bd61
Djmanny <Djmanny@RISRISH20030688.central.co.th>
2026-04-18 12:35:47 +0700

    Secure Sync: UI & Data Update (Selective Push)

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// Automatically generated at 2026-04-17 18:25:32
+// Automatically generated at 2026-04-18 12:35:46
 const DASHBOARD_DATA = {
     "Branch": {
         "1.1": {
@@ -241,4 +241,4 @@ const DASHBOARD_DATA = {
         }
     }
 };
-const LAST_UPDATED = '2026-04-17 18:25:32';
+const LAST_UPDATED = '2026-04-18 12:35:46';
